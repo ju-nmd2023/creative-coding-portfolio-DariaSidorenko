5c4d6f08bed9f67a36c3492726606959bb07c338
DariaSidorenko <dariasidorenko5@gmail.com>
2025-09-04 21:18:35 +0200

    updating to colour gradient

--- experiments/garritnoise2.js
+++ experiments/garritnoise2.js
@@ -1,21 +1,33 @@
 function setup() {
     createCanvas(600, 600);
+    frameRate(5);
 }
 
-const size = 10;
-const divider = 60;
+const size = 15;
+const divider = 30;
 const numRows = 60;
 const numCols = 60;
 
+let counter = 0;
+
 function draw() {
-    background(255, 255, 255);
-    noStroke();
-    fill(0, 0, 0);
+    background(0);
+    //noStroke();
+    //fill(0, 0, 0);
 
     for (let y = 0; y < numRows; y++) {
         for (let x = 0; x < numCols; x++) {
-            ellipse(x * size, y * size, size);
+            const r = noise(x / divider, y / divider, counter) * 255;
+            const g = noise(x / divider + 100, y / divider + 100, counter) * 255;
+            const b = noise(x / divider + 200, y / divider + 200, counter) * 255;
+
+            fill(r, g, b);
+            noStroke();
+
+            const value = noise(x / divider, y / divider, counter) * size;
+            ellipse(size / 2 + x * size, size / 2 + y * size, value);
         }
     }
-    noLoop();
+
+    counter += 0.1;
 }
